05da04ba1a9ffb1e0369e022f910f526f73c441c
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-12-09 16:33:44 +0100

    lab setup

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello {{ name }}</h1>
+
+  <button @click="helloWorld()">Button</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let name = ref("Cristina");
+
+function helloWorld() {
+  console.log("Hello world");
+}
+</script>
+<style></style>
